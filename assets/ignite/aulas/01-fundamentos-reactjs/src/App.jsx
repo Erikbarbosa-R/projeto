6bd4ebac97eea702f33e8d6fb9a2139c0eba9442
Erik Barbosa <Erikbarbosa5283@gmail.com>
2023-10-26 01:02:23 -0300

    :tada: initial commit

---
+++ assets/ignite/aulas/01-fundamentos-reactjs/src/App.jsx
@@ -0,0 +1,14 @@
+import { Post } from './Post';
+
+ function App() {
+  return (
+    <div>
+    <Post 
+      author="diego fernandes" 
+      content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit esse et excepturi cum corrupti accusamus vel nulla aut incidunt, minus, repellat fuga rerum? Inventore alias consectetur sint repudiandae libero! Repudiandae!"  
+    />
+  
+    </div>
+  )
+}
+export default App
